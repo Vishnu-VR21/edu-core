08e81e2a134263cd3a88900c51a883c149bc92bc
Vishnu V R <79919834+Vishnu-VR21@users.noreply.github.com>
2026-04-01 19:58:34 +0530

    Update config to comment out local URLs
    Comment out local baseurl and imgurl for production use

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,6 +1,9 @@
 const config = {
   baseurl: process.env.REACT_APP_BASE_URL,
   imgurl: process.env.REACT_APP_IMG_URL,
+
+  // baseurl: "http://127.0.0.1:8000/", 
+  // imgurl: "http://127.0.0.1:8000",
 };
 
 export default config;
